da0909664be41cfef2b53bf938cdb33e683d0a83
Paulo Ferreira <phgferreira@outlook.com>
2022-09-12 22:36:24 -0300

    Criando um testador de lista de cursos

---
+++ src/ExemploCursos.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class ExemploCursos {
+
+	public static void main(String[] args) {
+		List<Curso> cursos = new ArrayList<>();
+		cursos.add(new Curso("Python", 45));
+		cursos.add(new Curso("JavaScript", 150));
+		cursos.add(new Curso("Java 8", 113));
+		cursos.add(new Curso("C", 55));
+	}
+
+}
