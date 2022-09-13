b435ba108c81055fc90691bd9b081b00530544f9
Paulo Ferreira <phgferreira@outlook.com>
2022-09-12 22:39:11 -0300

    Usando method references na lista de cursos

--- src/ExemploCursos.java
+++ src/ExemploCursos.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 public class ExemploCursos {
@@ -9,6 +10,9 @@ public class ExemploCursos {
 		cursos.add(new Curso("JavaScript", 150));
 		cursos.add(new Curso("Java 8", 113));
 		cursos.add(new Curso("C", 55));
+		
+		cursos.sort(Comparator.comparing(Curso::getQuantidadeAlunos));
+		cursos.forEach(System.out::println);
 	}
 
 }
