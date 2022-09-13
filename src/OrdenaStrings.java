f02c3b30529712978a2d23f533bc98fdc31f0faa
Paulo Ferreira <phgferreira@outlook.com>
2022-09-12 21:18:59 -0300

    Criando um Consumer para iterar os elementos de uma lista

--- src/OrdenaStrings.java
+++ src/OrdenaStrings.java
@@ -1,6 +1,7 @@
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
+import java.util.function.Consumer;
 
 public class OrdenaStrings {
 
@@ -19,9 +20,12 @@ public class OrdenaStrings {
 		palavras.sort(comparador);
 		System.out.println(palavras);
 		
-		for (String palavra : palavras) {
-			System.out.println(palavra);
-		}
+//		for (String palavra : palavras) {
+//			System.out.println(palavra);
+//		}
+		
+		Consumer<String> consumidor = new ImprimeNaLinha();
+		palavras.forEach(consumidor);
 	}
 
 }
@@ -35,6 +39,15 @@ class ComparadorPorTamanho implements Comparator<String> {
 		if (s1.length() > s2.length())
 			return 1;
 		return 0;
+	}
+	
+}
+
+class ImprimeNaLinha implements Consumer<String> {
+
+	@Override
+	public void accept(String string) {
+		System.out.println(string);
 	}
 	
 }
