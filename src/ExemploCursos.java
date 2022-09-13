01f98c1cafc87e123bbb76017806e38e6f1da619
Paulo Ferreira <phgferreira@outlook.com>
2022-09-12 22:47:14 -0300

    Usando os métodos encadeados da classe Stream para filtrar

--- src/ExemploCursos.java
+++ src/ExemploCursos.java
@@ -12,7 +12,14 @@ public class ExemploCursos {
 		cursos.add(new Curso("C", 55));
 		
 		cursos.sort(Comparator.comparing(Curso::getQuantidadeAlunos));
-		cursos.forEach(curso -> System.out.println(curso.getNome()));
+		
+		cursos.stream()
+			// Esse trecho de código utiliza o design pattern chamado encadeamento
+			.filter(curso -> curso.getQuantidadeAlunos() >= 100)
+			.forEach(curso ->  System.out.println(curso.getNome()));
+		
+		// O filter feito acima não impacta no objeto original, por isso precisou usar o for each encadeado acima
+		//cursos.forEach(curso -> System.out.println(curso.getNome()));
 	}
 
 }
