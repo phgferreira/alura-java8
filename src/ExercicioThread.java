c07db91e75bb175614c5fde5acf64e73953b92aa
Paulo Ferreira <phgferreira@outlook.com>
2022-09-12 21:59:29 -0300

    Realizado exercício

--- src/ExercicioThread.java
+++ src/ExercicioThread.java
@@ -3,14 +3,8 @@ public class ExercicioThread {
 
 	public static void main(String[] args) {
 		
-		new Thread(new Runnable() {
-
-		    @Override
-		    public void run() {
-		        System.out.println("Executando um Runnable");
-		    }
-
-		}).start();
+		new Thread(() -> System.out.println("Executando um Runnable"))
+			.start();
 
 	}
 	
