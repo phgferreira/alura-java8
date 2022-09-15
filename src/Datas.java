e29844fa70554f6983b9717bd756ab4e72873e71
Paulo Ferreira <phgferreira@outlook.com>
2022-09-14 21:05:02 -0300

    Subtraindo anos de duas datas diferentes

--- src/Datas.java
+++ src/Datas.java
@@ -1,10 +1,16 @@
 import java.time.LocalDate;
+import java.time.Month;
 
 public class Datas {
 
 	public static void main(String[] args) {
 		LocalDate hoje = LocalDate.now();
 		System.out.println(hoje);
+		
+		LocalDate proximoAniversario = LocalDate.of(2023, Month.SEPTEMBER, 13);
+		
+		int anos = proximoAniversario.getYear() - hoje.getYear();
+		System.out.println(anos);
 	}
 
 }
